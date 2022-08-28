e0617c7d63e8a7eddfb967e6cb3457904f05304e
Mike Rugh <mike.rugh@gmail.com>
2022-08-28 11:08:49 -0700

    modifies delete route

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,6 +1,6 @@
 const notes = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
-const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+const { readAndAppend, readFromFile, writeToFile } = require('../helpers/fsUtils');
 const fs = require('fs');
 // const activeNotes = require('../db/db.json');
 
@@ -36,35 +36,33 @@ notes.post('/', (req, res) => {
   }
 });
 
-//LET'S TRY MAKING OUR OWN readAndDelete() based on readAndAppend() in fsUtils
+//DELETE route for deleting selected note
 notes.delete('/:id', (req, res) => {
-    // let activeNotes = readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
-    let activeNotes = require('../db/db.json');
-    console.log('activeNotes:', activeNotes);
-    const found = activeNotes.some(activeNotes => activeNotes.id === req.params.id);
-    
-    if (!found) {
-      res.status(400).json({ msg: `No notes with id of ${req.params.id}` });
-    } else {
-        deleteObj(activeNotes, req.params.id);
-    }
 
-    function deleteObj(data, id) {
-        for (i = 0; i < data.length; i++) {
-            if (data[i].id == id) {
-                data.splice(i,1);
-                data = JSON.stringify(data);
-                fs.writeFile('./db/db.json', data, (err) => {
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        console.log("Success!");
-                    }
-                });
+    const deleteObj = (id) => {
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+              console.error(err);
+            } else {
+                let parsedData = JSON.parse(data);
+                for (i = 0; i < parsedData.length; i++) {
+                    if (parsedData[i].id == id) {
+                        parsedData.splice(i,1);
+                        writeToFile('./db/db.json', parsedData);
+
+                        const response = {
+                            status: 'success',
+                            body: data,
+                        };
+                    
+                        res.json(response);
+                    } 
+                }
             }
         }
-    }
-    
+    )}
+
+    deleteObj(req.params.id);
 });
 
 
